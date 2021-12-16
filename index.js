d7c6b6cf3d45c0d366049698a1492d38a270a34f
tunguyen92 <tunguyen3692@gmail.com>
2021-12-16 17:27:05 +0700

    deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const options = {
 
     servers: [
       {
-        url: "https://database.giasumantiep.com/",
+        url: "https://giasumantiep-backend.herokuapp.com/",
       },
       {
         url: "http://localhost:8080",
